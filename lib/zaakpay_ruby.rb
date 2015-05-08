051124862914813cb53bf65edb00a0bde17e2c5a
Shoaib Malik <shoaib2109@gmail.com>
2015-05-08 15:38:09 +0530

    name issue fixed

--- lib/zaakpay_ruby.rb
+++ lib/zaakpay_ruby.rb
@@ -32,10 +32,10 @@ module ZaakpayRuby
     params_hash.each {|key, value|
       paramsstring += "'" + value.to_s + "'"
     }
-    checksum = OpenSSL::HMAC.hexdigest('sha256', Zaakpay.configuration.secret_key, paramsstring)
+    checksum = OpenSSL::HMAC.hexdigest('sha256', ZaakpayRuby.configuration.secret_key, paramsstring)
   end
 
-  # This is a helper method for generating Zaakpay checksum
+  # This is a helper method for generating ZaakpayRuby checksum
   # It sorts the parameters hash in ascending order of the keys
   # arguments: a parameters hash
   # return value: a hash with sorted in ascending order of keys
@@ -51,7 +51,7 @@ module ZaakpayRuby
 
 
   #
-  # This class is for wrappers around the Zaakpay request.
+  # This class is for wrappers around the ZaakpayRuby request.
   #
   # Checksum is generated in the constructor, so
   #  once instantiated, object.checksum gives the value of the checksum
@@ -66,7 +66,7 @@ module ZaakpayRuby
 
     def initialize(args_hash)
       @params = args_hash
-      @checksum = Zaakpay.generate_checksum(@params)
+      @checksum = ZaakpayRuby.generate_checksum(@params)
       @all_params = {}.merge(@params).merge({'checksum' => @checksum })
     end
 
@@ -92,7 +92,7 @@ module ZaakpayRuby
     end
 
     def valid?
-      @checksum = Zaakpay.generate_checksum(@params)
+      @checksum = ZaakpayRuby.generate_checksum(@params)
       @posted_checksum == @checksum
     end
 
